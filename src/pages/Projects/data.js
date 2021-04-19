3bb52dc13a9ba7df667da2e593234740349c3dfb
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-19 19:47:34 +0100

    update seeds to have ids

---
+++ src/pages/Projects/data.js
@@ -0,0 +1,31 @@
+const projectData = [
+    { 
+        id: 1,
+        name: "Habité",
+        description: "",
+        github: "https://github.com/natbibi/gossip-girl-xoxo",
+        netlify: "https://habite.netlify.app/",
+        image: "http:"
+    },
+
+    { 
+        id: 2,
+        name: "Gossip Girls",
+        description: "",
+        github: "https://github.com/natbibi/gossip-girl-xoxo",
+        netlify: "https://gossip-girl-xoxo.netlify.app/",
+        image: "http:"
+    },
+
+    { 
+        id: 3,
+        name: "Justin Bieber Fan Page",
+        description: "",
+        github: "https://github.com/natbibi/music-react",
+        netlify: "https://beliebers.netlify.app/",
+        image: "http:"
+    },
+
+];
+
+export default projectData;
