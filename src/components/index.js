462aefd6f7d8914c2c68b3d17956e616db34c7fc
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-19 19:49:07 +0100

    correct file structure for ProjectCard component

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,2 @@
 export { default as BackButton } from './BackButton';
+export { default as ProjectCards } from './ProjectCards';
--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
+import { ProjectCards } from '../../components'
+import projectData from './data'
 
-const Projects = () => <h1>My Projects</h1>;
+const Projects = () => {
+
+    const Project = projectData.map((projectData) => (
+        <ProjectCards key={projectData.id} projectData={projectData} />
+    ))
+
+    return (
+        <>
+        <h1>My Projects</h1>
+        {Project}
+        </>
+    )
+}
 
 export default Projects;
