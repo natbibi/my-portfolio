648148ab03114f21af65324f668c50e1894586e4
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-20 09:21:27 +0100

    start on implementing carousel for pprojects

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ProjectCards } from '../../components'
 import projectData from './data'
 
 const Projects = () => {
 
     const Project = projectData.map((projectData) => (
-        <ProjectCards key={projectData.id} projectData={projectData} />
+        <ProjectCards key={projectData.id} className="project-container" projectData={projectData} />
     ))
 
     return (
         <>
-        <h1>My Projects</h1>
-        {Project}
+            <h1>My Projects</h1>
+            <div className="slider">
+                {Project}
+            </div>
         </>
     )
 }
