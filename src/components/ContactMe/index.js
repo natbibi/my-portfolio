972bf620f1ecb3abe64f5e31bf9c4e9614eedf37
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-22 13:42:55 +0100

    contact page with personal details filled out plus styling

--- src/components/ContactMe/index.js
+++ src/components/ContactMe/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import './style.css'
 
 const ContactMe = () => {
 
 
     return (
+
 
         <div class="container">
 
@@ -11,7 +13,7 @@ const ContactMe = () => {
                 <div class="face face1">
                     <div class="content">
                         <div class="icon">
-                            <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+                        <i class="fas fa-envelope-square" aria-hidden="true"></i>
                         </div>
                     </div>
                 </div>
@@ -19,9 +21,9 @@ const ContactMe = () => {
                 <div class="face face2">
                     <div class="content">
                         <h3>
-                            <a href="https://www.linkedin.com/in/adamdipinto/" target="_blank">_adamdipinto</a>
+                            <a href="mailto: natalie.pang@me.com" target="_blank">natalie.pang@me.com</a>
                         </h3>
-                        <p>This is where I network and build my professional protfolio.</p>
+                        <p>This is my personal email if you have questions or job opportunities.</p>
                     </div>
                 </div>
             </div>
@@ -38,9 +40,9 @@ const ContactMe = () => {
                 <div class="face face2">
                     <div class="content">
                         <h3>
-                            <a href="https://twitter.com/AdamDipinto" target="_blank">@AdamDipinto</a>
+                            <a href="https://twitter.com/natalieyehh" target="_blank">@natalieyehh</a>
                         </h3>
-                        <p>This is where I read news and network with different social groups.</p>
+                        <p>This is where I follow my favourite topics and network with different social groups.</p>
                     </div>
                 </div>
             </div>
@@ -56,14 +58,14 @@ const ContactMe = () => {
                 <div class="face face2">
                     <div class="content">
                         <h3>
-                            <a href="https://github.com/atom888" target="_blank">atom888</a>
+                            <a href="https://github.com/natbibi" target="_blank">@natbibi</a>
                         </h3>
                         <p>This is where I share code and work on projects.</p>
                     </div>
                 </div>
             </div>
 
-    </div>
+      </div>
 
 
     )
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
+import { ContactMe } from '../../components'
 
 const Contact = () => {
-
 
     return (
     <>
@@ -9,7 +9,7 @@ const Contact = () => {
             <h1>💌 Contact</h1>
             <h5>A few ways to get in touch with me.</h5>
         </div>  
-        {ContactMe()}
+        <ContactMe />
     </>
     )
 
