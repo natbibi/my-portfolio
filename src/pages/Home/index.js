2390f9be1d91e4915c2c3187938e81f6f379e418
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-22 19:08:08 +0100

    set up about page with intro and skills coming soon

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
 const Home = () =>
-    <div className="intro-container">
-        <h1>👋🏼 About Me</h1>
-        <h5>Since graduating from my MSc in International Marketing and BSc in Business Management, I am currently training to be full-stack Software Engineer with futureproof.</h5>
-    </div>
+    <>
+        <header className="intro-container">
+            <h1>👋🏼 About Me</h1>
+            <h5>Hello! I am a Business/Marketing grad currently training to be full-stack Software Engineer with futureproof. I discovered my biggest passions (so far) in piano 🎹,
+            active investing 📈and yoga 🧘🏻‍♀️. I live to travel and love to see the world 🌍 (29 countries and counting!). </h5>
+        </header>
+        <main className="projects-container">
+    <h2 className="center-align">Skills</h2>
+    <p className="center-align">Coming Soon...</p>
+        </main>
+    </>
 export default Home;
