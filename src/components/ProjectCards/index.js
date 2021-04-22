e37ecf8305d68fce3ee5a6f40b3c377539f3f4f8
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-22 16:01:44 +0100

    update order or items, add classNames, and add description to page

--- src/components/ProjectCards/index.js
+++ src/components/ProjectCards/index.js
@@ -9,6 +9,7 @@ const ProjectCards = ({ projectData }) => {
     <div className="project-container">
       <div>
         <h3>{projectData.name}</h3>
+        <p className="project-description">{projectData.description}</p>
         <button className="visit-website"><a href={projectData.netlify} target="_blank">Visit Website  <i className="fas fa-external-link-alt"></i></a></button>
         <button className="visit-repo"><a href={projectData.github} target="_blank">Visit Repo  <i className="fas fa-external-link-alt"></i></a></button>
       </div>
