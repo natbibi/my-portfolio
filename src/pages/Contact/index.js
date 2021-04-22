180c1623a57671fbdeea8ea211e12178e4a60cb1
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-22 12:43:58 +0100

    create heading container for contact

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,5 +1,18 @@
 import React from 'react';
 
-const Contact = () => <h1>Contact Me</h1>;
+const Contact = () => {
+
+
+    return (
+    <>
+        <div className="intro-container">
+            <h1>💌 Contact</h1>
+            <h5>A few ways to get in touch with me.</h5>
+        </div>  
+        {ContactMe()}
+    </>
+    )
+
+}
 
 export default Contact;
