35627cc3845784aba32713f294e914e0bb2b1fcc
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-22 10:19:24 +0100

    add seeds and update some data

--- src/pages/Projects/data.js
+++ src/pages/Projects/data.js
@@ -1,29 +1,42 @@
 const projectData = [
     { 
         id: 1,
-        name: "Habité",
-        description: "",
-        github: "https://github.com/natbibi/gossip-girl-xoxo",
-        netlify: "https://habite.netlify.app/",
-        image: "http:"
-    },
-
-    { 
-        id: 2,
         name: "Gossip Girls",
         description: "",
+        technologies: "",
         github: "https://github.com/natbibi/gossip-girl-xoxo",
         netlify: "https://gossip-girl-xoxo.netlify.app/",
         image: "http:"
     },
 
     { 
+        id: 2,
+        name: "Habité",
+        description: "",
+        technologies: "",
+        github: "https://github.com/natbibi/co-habiters",
+        netlify: "https://habite.netlify.app/",
+        image: "https://user-images.githubusercontent.com/58271566/114206025-cf58bf80-9952-11eb-9d02-2afd378417ab.png"
+    },
+
+    { 
         id: 3,
         name: "Justin Bieber Fan Page",
         description: "",
+        technologies: "",
         github: "https://github.com/natbibi/music-react",
         netlify: "https://beliebers.netlify.app/",
         image: "http:"
+    },
+
+    { 
+        id: 4,
+        name: "Github Repo Finder",
+        description: "",
+        technologies: "",
+        github: "https://github.com/cwesterduin/challenge-github",
+        netlify: "https://better-github.netlify.app/",
+        image: "https://user-images.githubusercontent.com/58271566/115594211-63207900-a2cd-11eb-9fd1-1efa2070d8d9.gif"
     },
 
 ];
