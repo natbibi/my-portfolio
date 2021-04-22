8ef71b93f208ff49498bb9706b6c3fb94c038ebb
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-22 12:43:34 +0100

    create about heading container

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
-const Home = () => <h1>Welcome</h1>;
-
+const Home = () =>
+    <div className="intro-container">
+        <h1>👋🏼 About Me</h1>
+        <h5>Since graduating from my MSc in International Marketing and BSc in Business Management, I am currently training to be full-stack Software Engineer with futureproof.</h5>
+    </div>
 export default Home;
