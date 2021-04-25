b4c0740afb67fc70cddb775232ca9a2dd0559b7a
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-25 21:45:02 +0100

    add techologies used to projects

--- src/components/ProjectCards/index.js
+++ src/components/ProjectCards/index.js
@@ -8,12 +8,13 @@ const ProjectCards = ({ projectData, handleSelect }) => {
       <div>
         <h3>{projectData.name}</h3>
         <p className="project-description">{projectData.description}</p>
-        <button className="visit-website"><a href={projectData.netlify} target="_blank">Visit Website  <i className="fas fa-external-link-alt"></i></a></button>
-        <button className="visit-repo"><a href={projectData.github} target="_blank">Visit Repo  <i className="fas fa-external-link-alt"></i></a></button>
+        <p className="project-technologies">{projectData.technologies}</p>
+        <button className="visit-website"><a href={projectData.netlify} target="_blank">Website  <i className="fas fa-external-link-alt"></i></a></button>
+        <button className="visit-repo"><a href={projectData.github} target="_blank">Repo  <i className="fas fa-external-link-alt"></i></a></button>
       </div>
 
       <div>
-        <img src={projectData.image} alt={projectData.alt} className="project-img" onClick={() => handleSelect(projectData.id)}/>  
+        <img src={projectData.image} alt={projectData.alt} className="project-img" onClick={() => handleSelect(projectData.id)} />
       </div>
 
     </main >
