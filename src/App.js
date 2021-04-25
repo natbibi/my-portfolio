e1c15b89bd02b75a7373ba251304aab2e286b6e9
natbibi <58271566+natbibi@users.noreply.github.com>
2021-04-25 20:40:40 +0100

    add NotFound

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom';
-import { Home, Projects, Contact } from './pages';
+import * as Pages from './pages';
 import { Header, Footer } from './layout'
 import profilepic from '../public/images/profilepic.jpg'
 
@@ -13,9 +13,18 @@ function App(){
         <Header />
         <img src={profilepic} alt="logo / Animated avatar of Natalie" className="img-circle" />
         <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/contact" component={Contact} />
+          <Route exact path="/">
+            <Pages.Home />
+          </Route>
+          <Route path="/projects">
+            <Pages.Projects />
+              </Route>
+          <Route path="/contact">
+            <Pages.Contact />
+          </Route>
+          <Route>
+            <Pages.NotFound />
+          </Route>
         </Switch>
         <Footer />
       </>
