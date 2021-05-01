ea2a1eaa20af7d988c4a143d77b00878c48694d7
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-01 23:11:24 +0100

    create new component for projects on main page

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as BackButton } from './BackButton';
 export { default as ContactMe } from './ContactMe';
+export { default as Project } from './Project';
 export { default as ProjectCards } from './ProjectCards';
 export { default as ScrollToTop } from './ScrollToTop';
