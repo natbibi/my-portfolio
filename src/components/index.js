d79e5b3b4e127dcacc3188819c1cd5997a19f1b1
natbibi <58271566+natbibi@users.noreply.github.com>
2021-05-03 13:04:47 +0100

    move skills/tech icons to a component

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as ContactMe } from './ContactMe';
 export { default as Project } from './Project';
 export { default as ProjectCards } from './ProjectCards';
 export { default as ScrollToTop } from './ScrollToTop';
+export { default as Skills } from './Skills';
