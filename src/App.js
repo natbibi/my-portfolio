3d2d21e1c18cb4a54cfa9a0cbde49585c1948afc
natbibi <58271566+natbibi@users.noreply.github.com>
2021-07-04 14:11:27 +0100

    :children_crossing: logo onClick goes to homepage

--- src/App.js
+++ src/App.js
@@ -1,38 +1,40 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, useHistory } from 'react-router-dom';
 import * as Pages from './pages';
 import { Burger, Footer } from './layout'
 import profilepic from '../public/images/profilepic.jpg'
 
 import './style.css';
 
-function App(){
+function App() {
 
-    return(
-      <>
-        <Burger />
-        <img src={profilepic} alt="logo / Animated avatar of Natalie" className="img-circle" />
-        <Switch>
-          <Route exact path="/">
-            <Pages.Home />
-          </Route>
-          <Route path="/about">
-            <Pages.About />
-          </Route>
-          <Route path="/projects">
-            <Pages.Projects />
-              </Route>
-          <Route path="/contact">
-            <Pages.Contact />
-          </Route>
-          <Route>
-            <Pages.NotFound />
-          </Route>
-        </Switch>
-        <Footer />
-      </>
-    );
-  
-  };
-  
-  export default App;
+  const history = useHistory();
+
+  return (
+    <>
+      <Burger />
+      <img src={profilepic} onClick={() => { history.push(`/`) }} alt="logo / Animated avatar of Natalie" className="img-circle" />
+      <Switch>
+        <Route exact path="/">
+          <Pages.Home />
+        </Route>
+        <Route path="/about">
+          <Pages.About />
+        </Route>
+        <Route path="/projects">
+          <Pages.Projects />
+        </Route>
+        <Route path="/contact">
+          <Pages.Contact />
+        </Route>
+        <Route>
+          <Pages.NotFound />
+        </Route>
+      </Switch>
+      <Footer />
+    </>
+  );
+
+};
+
+export default App;
